92aaa7379ecf1d63d1cbf7f8db7902fd1482dfb9
A-Dummo <2014adamw@gmail.com>
2022-03-08 00:34:10 -0800

    Put more functions

--- script.js
+++ script.js
@@ -15,22 +15,93 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch(operator) {
         case "+":
-            return a + b;
+            return add(a, b);
 
         case "-":
-            return a - b;
+            return subtract(a, b);
         
         case "*":
-            return a * b;
+            return multiply(a, b);
         
         case "/":
-            return a / b;
+            return divide(a, b);
     }
 }
 
+function doNumstuff(display, dataKeyValue) {
+    switch(dataKeyValue) {
+        case "C":
+            display.textContent = 0;
+            break;
 
+        case "=":
+            let arrayNums = splitArray(display.textContent);
+            display.textContent = addArrayNums(arrayNums);
+            break;
+
+        default:
+            display.textContent += dataKeyValue;
+            break;
+    }
+}
+
+function splitArray(x) {
+    let y = [""]
+    let c = 0;
+    for (let i = 0; i < x.length; i++) {
+    
+        if (x[i] === "/" || x[i] === "*" || x[i] === "-" || x[i] === "+") {
+            
+            c += 1;
+            y[c]="";
+    
+        } else if (y[c] === "/" || y[c] === "*" || y[c] === "-" || y[c] === "+") {
+            c += 1;
+            y[c]="";
+        }
+    
+        y[c]+=x[i];
+    }
+    return y;
+}
+
+function addArrayNums(arrayNums) {
+    for (let i = 0; i < arrayNums.length - 2; i += 2) {
+        // 1, +, 3, -, 2
+        arrayNums[i + 2] = operate(arrayNums[i + 1], arrayNums[i], arrayNums[i + 2]);
+    }
+    const sum = arrayNums[arrayNums.length - 1];
+    return sum;
+}
 // init
 
-//let buttons = document.querySelectorAll("");
+let buttons = document.querySelectorAll("#buttons > *");
+
+buttons.forEach(element => {
+    
+    element.addEventListener('click', () => {
+        //document.querySelector("#display").textContent += x.getAttribute("data-key")
+        const dataKeyValue = element.getAttribute("data-key");
+        const display = document.querySelector("#display");
+        /*
+        switch(dataKeyValue) {
+            case "C":
+                display.textContent = 0;
+                break;
+
+            case "=":
+                console.log(1)
+                break;
+
+            default:
+                display.textContent += dataKeyValue;
+                break;
+        }
+        */
+       doNumstuff(display, dataKeyValue);
+    })
+})
